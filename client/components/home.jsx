dad8ad5e34d650c89303d2c6269c8b6b5dfc9858
douglasmccuen <doug@talool.com>
2017-01-20 12:01:19 -0700

    test

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -11,6 +11,7 @@ class Home extends React.Component {
         <h1>Hello <a href={"https://github.com/electrode-io"}>{"Electrode"}</a></h1>
         <div>
           <h2>Managing States with Redux</h2>
+          <h2>Kemosabe7</h2>
           <label>
             <input onChange={props.onChangeCheck} type={"checkbox"} checked={checked}/>
             Checkbox
